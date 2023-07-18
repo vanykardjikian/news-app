ed6e7be5a6b814d48a7634f637eff043e5fe7222
Vee1807 <125376159+Vee1807@users.noreply.github.com>
2023-07-18 20:43:15 +0400

    Change API to NewYorkTimes

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const API_KEY = "633256b667014be6b48a1bf2f01f5450";
-const url = "https://newsapi.org/v2/"
+const API_KEY = "gJY8ngGxHYmHmclHvvIEBkoxsEYs7WwM";
+const CATEGORY_URL = "https://api.nytimes.com/svc/topstories/v2/"
+const SEARCH_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
 
-window.addEventListener('load', () => fetchNews("armenia"))
+window.addEventListener('load', () => fetchNewsByCategory('home'))
 
-async function fetchNews(query) {
-    const res = await fetch(`${url}everything?q=${query}&apiKey=${API_KEY}`)
+async function fetchNewsByQuery(query) {
+    res = await fetch(`${SEARCH_URL}${query}&api-key=${API_KEY}`)
     const data = await res.json()
-    bindData(data.articles)
+    // If from search => 1, category => 0
+    bindData(data.response.docs, 'search')
+}
+
+async function fetchNewsByCategory(category) {
+    res = await fetch(`${CATEGORY_URL}${category}.json?api-key=${API_KEY}`)
+    const data = await res.json()
+    bindData(data.results, 'cat')
 }
 
 
-function bindData(articles) {
+function bindData(articles, flag) {
     window.scrollTo(0, 0);
     const cardsContainer = document.querySelector('.cards-container')
     const newsCardTemplate = document.getElementById('news-card-template')
@@ -18,29 +26,41 @@ function bindData(articles) {
     cardsContainer.innerHTML = ''
 
     articles.forEach(article => {
-        if(!article.urlToImage) return
+        if(!article.abstract) return
         const cardClone = newsCardTemplate.content.cloneNode(true)
-        fillCardData(cardClone, article)
+        fillCardData(cardClone, article, flag)
         cardsContainer.appendChild(cardClone)
     });
 }
 
-function fillCardData(card, article) {
+function fillCardData(card, article, flag) {
     const newsImage = card.querySelector('#news-img')
     const newsTitle = card.querySelector('.news-title')
     const newsSource = card.querySelector('.news-source')
     const newsParagraph = card.querySelector('.news-paragraph')
+    
+    if (flag === 'search') {
+        const date = new Date(article.pub_date).toLocaleDateString();
+        newsImage.src = `https://static01.nyt.com/${article.multimedia[0].url}`
+        newsTitle.innerHTML = article.headline.main
+        newsSource.innerHTML = `${article.section_name} - ${date}`
+        newsParagraph.innerHTML = article.abstract
 
-    const date = new Date(article.publishedAt).toLocaleDateString();
+        card.firstElementChild.addEventListener('click', () => {
+            window.open(article.web_url, '_blank')
+        })
+    } else {
+        const date = new Date(article.published_date).toLocaleDateString();
+        newsImage.src = article.multimedia[0].url
+        newsTitle.innerHTML = article.title
+        newsSource.innerHTML = `${article.section} - ${date}`
+        newsParagraph.innerHTML = article.abstract
 
-    newsImage.src = article.urlToImage
-    newsTitle.innerHTML = article.title
-    newsSource.innerHTML = `${article.source.name} - ${date}`
-    newsParagraph.innerHTML = article.description
+        card.firstElementChild.addEventListener('click', () => {
+            window.open(article.url, '_blank')
+        })
+    }
 
-    card.firstElementChild.addEventListener('click', () => {
-        window.open(article.url, '_blank')
-    })
 }
 
 const searchBtn = document.querySelector('.search-button')
@@ -53,20 +73,20 @@ searchBtn.addEventListener('click', () => {
     const searchQuery = searchInput.value
     searchInput.value = ''
     if (!searchQuery) return
-    document.querySelector('#armenia').classList.remove('active')
+    document.querySelector('#home').classList.remove('active')
     if(currentNavItem) currentNavItem.classList.remove('active')
 
     searchResult.innerHTML = `Showing results for '${searchQuery}' ...`
-    fetchNews(searchQuery)
+    fetchNewsByQuery(searchQuery)
 })
 
 
-function toggleNavItem(query) {
+function toggleNavItem(category) {
     searchResult.innerHTML = ""
-    fetchNews(query)
-    document.querySelector('#armenia').classList.remove('active')
+    fetchNewsByCategory(category)
+    document.querySelector('#home').classList.remove('active')
     if(currentNavItem) currentNavItem.classList.remove('active')
-    currentNavItem = document.getElementById(query)
+    currentNavItem = document.getElementById(category)
     currentNavItem.classList.add('active')
 }
 
